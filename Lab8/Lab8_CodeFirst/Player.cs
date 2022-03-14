809e72147410310bde60ff4fc159c3190b3aaf7a
StevenK418 <stevenkelly418@gmail.com>
2022-03-14 11:10:58 +0000

    => Added Player Class. => Added Team Class. => Added Entity Framework packages.

---
+++ Lab8/Lab8_CodeFirst/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_CodeFirst
+{
+    class Player
+    {
+        public int PlayerID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int TeamID { get; set; }
+        public virtual Team Team { get; set; }
+    }
+}
---
+++ Lab8/Lab8_CodeFirst/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_CodeFirst
+{
+    class Team
+    {
+
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public string Location { get; set; }
+        public virtual List<Player> Players { get; set; }
+        
+    }
+}
